82985b5747bdc2364512647b96072a4c94594b9a
xingzheng <xingzheng@hotmail.com>
2019-08-13 23:02:32 +0800

    Add project files.

---
+++ Hubs/TestCaseHub.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.FileProviders;
+
+namespace SignalRTest.Hubs
+{
+    public class TestCaseHub : Hub
+    {
+        public async Task SendTestCase()
+        {
+            var provider = new PhysicalFileProvider(Directory.GetCurrentDirectory());
+
+            var fileInfos = provider.GetDirectoryContents(string.Empty);
+
+            foreach(var fileInfo in fileInfos)
+            {
+                using (var stream = fileInfo.CreateReadStream())
+                { 
+                    await Clients.All.SendAsync("ReceiveTestCase", fileInfo.Name, stream);
+                }
+            }
+        }
+    }
+}
