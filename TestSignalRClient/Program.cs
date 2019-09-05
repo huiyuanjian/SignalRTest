7a1cb0a02750e6ab09bf8bd1738ec31471e7109e
xingzheng <xingzheng@outlook.com>
2019-09-05 15:42:47 +0800

    Add SignalR test server and  client

---
+++ TestSignalRClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace TestSignalRClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HubConnection connection = new HubConnectionBuilder().WithUrl("https://localhost:44306/TestCaseHub").Build();
+
+            connection.Closed += async (error) =>
+            {
+                await Task.Delay(new Random().Next(0, 5) * 1000);
+                await connection.StartAsync();
+            };
+
+            connection.On<string, string>("ReceiveString", (fileName, content) =>
+            {
+                Console.WriteLine($"Recieve file:{fileName}");
+                using (var fileStream = new StreamWriter(fileName))
+                {
+                    fileStream.Write(content);
+                }
+            });
+
+            connection.On("TestReceive", () =>
+            {
+                Console.WriteLine("Recieve sucessfully!");
+            });
+
+            connection.StartAsync();
+            Console.WriteLine("Start connect");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TestSignalRServer/Hubs/TestCaseHub.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.FileProviders;
+
+namespace TestSignalRServer
+{
+    public class TestCaseHub : Hub
+    {
+        public async Task SendTestCase(string fileName)
+        {
+            var provider = new PhysicalFileProvider(Directory.GetCurrentDirectory());
+
+            var fileInfos = provider.GetDirectoryContents("TestCases");
+            var fileInfo = fileInfos.FirstOrDefault(f => f.Name.Contains(fileName));
+
+            using (var reader = new StreamReader(fileInfo.PhysicalPath))
+            {
+                await Clients.All.SendAsync("TestReceive");
+                var content = reader.ReadToEnd();
+                await Clients.All.SendAsync("ReceiveString", fileInfo.Name, content);
+            }
+
+        }
+    }
+}
